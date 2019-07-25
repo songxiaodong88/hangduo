1a3889a63dc2a251536fc2d74e66ae073c5ce873
xin <xlglxzq@163.com>
2019-07-25 14:43:09 +0800

    初始化项目

---
+++ src/main/java/com/hangduo/airplane/knowledgemanagement/KnowledgemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hangduo.airplane.knowledgemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgemanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hangduo/airplane/knowledgemanagement/KnowledgemanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hangduo.airplane.knowledgemanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowledgemanagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
